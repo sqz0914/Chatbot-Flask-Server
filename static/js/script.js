c31104ccd20b50786d6cbbc98d2de9d7ac4c1907
sqz0914 <sqz0914@gmail.com>
2024-05-07 15:43:10 -0700

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -11,7 +11,11 @@ function sendMessage() {
         url: '/chat/1x2abc/1',
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify({ user_input: userInput }),
+        data: JSON.stringify({ 
+            user_input: userInput,
+            message_id: uuidv4(),
+            timestamp: Date.now().toLocaleString()
+        }),
         success: function(data) {
             $('#chatbox').append('<p><strong>You:</strong> ' + userInput + '</p>');
             $('#chatbox').append('<p><strong>Bot:</strong> ' + data.response + '</p>');
@@ -23,3 +27,8 @@ function sendMessage() {
     });
 }
 
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+}
